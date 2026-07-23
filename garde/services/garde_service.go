f0f7bc5133bdf0ef205820d1bfbbbad0052037c6
Kassi Fulgence <Line89034@gmail.com>
2026-07-23 19:01:11 +0200

    correction

--- garde/services/garde_service.go
+++ garde/services/garde_service.go
@@ -1,11 +1,25 @@
 package services
 
 import (
+	"errors"
 	"pharmacie-api/garde/models"
 	"pharmacie-api/garde/repositories"
 )
 
 func AjouterGardeService(garde models.Garde) error {
+
+	if garde.DateGarde == "" {
+		return errors.New("Le champ Date est obligatoires")
+	}
+	if garde.Nom_Garde == "" {
+		return errors.New("Le champ Nom est obligatoire")
+	}
+	if garde.HeureDebut == "" {
+		return errors.New("Le champ Heure de debut est obligatoire")
+	}
+	if garde.HeureFin == "" {
+		return errors.New("Le champ Heure de fin est obligatoire")
+	}
 	return repositories.AjouterGardeDB(garde)
 }
 
